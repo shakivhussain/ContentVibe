d886b55eaf07828bc52a8784cf0968a7aecc6a9e
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-08-02 23:50:37 +0530

    ✅ Hide Bottom Navigation Write Post Screen

--- app/src/main/java/com/shakiv/husain/instagramui/ui/InstagramApp.kt
+++ app/src/main/java/com/shakiv/husain/instagramui/ui/InstagramApp.kt
@@ -1,29 +1,27 @@
 package com.shakiv.husain.instagramui.ui
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.shakiv.husain.instagramui.data.AppContainer
+import com.shakiv.husain.instagramui.ui.app.AddPostDestination
 import com.shakiv.husain.instagramui.ui.app.HomeDestination
 import com.shakiv.husain.instagramui.ui.app.InstagramNavHost
 import com.shakiv.husain.instagramui.ui.app.bottomTabRowScreens
 import com.shakiv.husain.instagramui.ui.app.navigateToSingleTopTo
 import com.shakiv.husain.instagramui.ui.components.BottomNavigationTabRow
-import com.shakiv.husain.instagramui.utils.IconsInstagram
-import com.shakiv.husain.instagramui.utils.ImageUtils
 
 @Composable
 fun InstagramApp(appContainer: AppContainer) {
@@ -41,12 +39,29 @@ fun InstagramAppContent(appContainer: AppContainer) {
     val currentBackStack by navController.currentBackStackEntryAsState()
     val currentDestination = currentBackStack?.destination
     val currentScreen = screens.find { it.route == currentDestination?.route } ?: HomeDestination
+    var bottomBarState by remember { mutableStateOf(true) }
+
+    bottomBarState = when (currentDestination?.route) {
+        AddPostDestination.route -> {
+            false
+        }
+
+        else -> {
+            true
+        }
+    }
 
     Scaffold(
         modifier = Modifier,
         bottomBar = {
-            BottomNavigationTabRow(screens, currentScreen) {newScreen->
-                navController.navigateToSingleTopTo(newScreen.route)
+            AnimatedVisibility(
+                visible = bottomBarState,
+                enter = slideInVertically(initialOffsetY = { it }),
+                exit = slideOutVertically(targetOffsetY = { it }),
+            ) {
+                BottomNavigationTabRow(screens, currentScreen) { newScreen ->
+                    navController.navigateToSingleTopTo(newScreen.route)
+                }
             }
         }
     ) { innerPadding ->
